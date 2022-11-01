313ba4d9cd87e8215ced285899b2396cc738e224
avelyy <dldkwls3221@naver.com>
2022-11-01 17:59:39 +0900

    Initial commit

---
+++ src/main/java/com/example/mysnack/MySnackApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysnack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySnackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MySnackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/mysnack/MySnackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysnack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySnackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
